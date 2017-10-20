9c2dd167b1e7d469cf5b1ba39c97c2d5f320da0d
Polygon <djblushberry@gmail.com>
2017-10-20 00:57:25 -0700

    Added some comments for easy reading.

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
-
 import os
 
+#Initialize TK and set it as the root window
 root = Tk()
 root.title("youtube-dl-tkinter")
 
@@ -9,15 +9,19 @@ class App:
   url = Label(root, text="Youtube URL: ").grid(row=0)
   e1 = Entry(root)
   e1.grid(row=0, column=1)
+  
+  #Initialize
   def __init__(self, master):
     frame = Frame(master)
     frame.grid()
-
+    
+    #Create Download Button
     self.command = Button(frame,
                          text="Download",
                          command=self.download)
     self.command.grid(column=1, row=0)
 
+  #This function uses youtube-dl to extract mp3 audio from a youtube url and formats in our entry text in e1
   def download(self):
     os.system("youtube-dl --extract-audio --audio-format mp3 {}".format(self.e1.get()))
     print(self.e1.get())
