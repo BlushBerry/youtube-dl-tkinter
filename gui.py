6b91681e9b7d38baffb3346325e63c27981cf74b
Polygon <djblushberry@gmail.com>
2017-10-26 14:36:47 -0700

    Added the ability to choose a download location

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import os
+from tkinter.filedialog import askdirectory
 
 #Initialize TK and set it as the root window
 root = Tk()
@@ -7,8 +8,12 @@ root.title("youtube-dl-tkinter")
 
 class App:
   url = Label(root, text="Youtube URL: ").grid(row=0)
+  path = Label(root, text="Download Path: ").grid(row=1)
+  chosenfolder = StringVar()
   e1 = Entry(root)
   e1.grid(row=0, column=1)
+  e2 = Entry(root, textvariable=chosenfolder)
+  e2.grid(row=1, column=1)
   
   #Initialize
   def __init__(self, master):
@@ -21,10 +26,22 @@ class App:
                          command=self.download)
     self.command.grid(column=1, row=0)
 
+    #Create File Path Button :v
+    self.command = Button(root,
+                         text="...",
+                         command=self.pickfolder)
+    self.command.grid(column=3, row=1)
+
   #This function uses youtube-dl to extract mp3 audio from a youtube url and formats in our entry text in e1
+  def pickfolder(self):
+    print("youtube-dl-tkinter: Choosing File Destination")
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askdirectory() # show an "Open" dialog box and return the path to the selected file
+    self.chosenfolder.set(filename)
+
   def download(self):
-    print("youtube-dl-tkinter: Initializing download of url {}".format(self.e1.get()))
-    os.system("youtube-dl --extract-audio --audio-format mp3 {}".format(self.e1.get()))
+    print("youtube-dl-tkinter: Initializing download of url {} to destination {}".format(self.e1.get(), self.chosenfolder.get()))
+    os.system("youtube-dl -o '{}/%(title)s.%(ext)s' --extract-audio --audio-format mp3 {}".format(self.chosenfolder.get(), self.e1.get()))
 
 app = App(root)
 root.mainloop()
